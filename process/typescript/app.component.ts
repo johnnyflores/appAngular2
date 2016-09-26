0d5fdb46aa2c3404720e3388fd9f1d86875ae812
johnny <johnnyflores@.live>
2016-09-26 16:50:41 -0400

    Creation directives to use in app

--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {MediaItemComponent} from './media-item.component';
 
 @Component( {
     selector: 'my-app',
+    directives: [MediaItemComponent],
     templateUrl: 'partials/app.component.html',
     styleUrls: ['css/app.component.css']
 })
---
+++ process/typescript/media-item.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component( {
+    selector: 'media-item',
+    templateUrl: 'partials/media-item.component.html',
+    styleUrls: ['css/media-item.component.css']
+})
+
+export class MediaItemComponent {}
