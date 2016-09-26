91fa09783e5c1a6246ecca1d4ab4ee197c3e4902
johnny <johnnyflores@.live>
2016-09-26 13:45:50 -0400

    Styling component

--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -2,7 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component( {
     selector: 'my-app',
-    templateUrl: 'partials/app.component.html'
+    templateUrl: 'partials/app.component.html',
+    styleUrls: ['css/app.component.css']
 })
 
 export class AppComponent {}
