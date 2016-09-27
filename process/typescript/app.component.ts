0df5819c790fff27c6c3232a3c6ee743536932d0
johnny <johnnyflores@.live>
2016-09-26 21:39:17 -0400

    Structural directives—ngFor

--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -1,25 +1,15 @@
 import {Component} from 'angular2/core';
 import {MediaItemComponent} from './media-item.component';
+import {MediaItemListComponent} from './media-item-list.component';
 
 @Component( {
     selector: 'my-app',
-    directives: [MediaItemComponent],
+    directives: [MediaItemComponent, MediaItemListComponent],
     templateUrl: 'partials/app.component.html',
     styleUrls: ['css/app.component.css']
 })
 
 export class AppComponent {
     onMediaItemDeleted(mediaItem) {
-
     }
-    firstMediaItem = {
-        id: 1,
-        name: "Firebug",
-        medium: "Series",
-        category: "Science Fiction",
-        year: 2010,
-        watchedOn: 1294166565384,
-        isFavorite: false
-    };
-
 }
---
+++ process/typescript/media-item-list.component.ts
@@ -0,0 +1,53 @@
+import {Component} from 'angular2/core';
+import {MediaItemComponent} from './media-item.component';
+
+@Component( {
+    selector: 'media-item-list',
+    directives: [MediaItemComponent],
+    templateUrl: 'partials/media-item-list.component.html',
+    styleUrls: ['css/media-item-list.component.css']
+})
+
+export class MediaItemListComponent {
+    onMediaItemDeleted(mediaItem) {
+
+    }
+    mediaItems = [
+    {
+        id: 1,
+        name: "Firebug",
+        medium: "Series",
+        category: "Science Fiction",
+        year: 2010,
+        watchedOn: 12944166565384,
+        isFavorite: false
+    },
+    {
+        id: 2,
+        name: "The small Tall",
+        medium: "Movies",
+        category: "Comedy",
+        year: 2015,
+        watchedOn: null,
+        isFavorite: true
+    },
+    {
+        id: 3,
+        name: "The Revolution",
+        medium: "Movies",
+        category: "Science Fiction",
+        year: 2016,
+        watchedOn: null,
+        isFavorite: false
+    },
+    {
+        id: 4,
+        name: "The Coporation",
+        medium: "Series",
+        category: "Documental",
+        year: 2012,
+        watchedOn: null,
+        isFavorite: true
+    }
+    ]
+}
